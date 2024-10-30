57e3ef4a57052a3053537a1d94f78cdf33b3ee89
PEDRO IVO FREITAS LAEBER <plaeber@labcetec.net>
2024-10-30 15:36:55 -0300

    Alteração 30/10

--- Cliente/script.js
+++ Cliente/script.js
@@ -1,10 +1,28 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
+    const loginButton = document.getElementById('loginBtn');
+    const errorMessage = document.getElementById('error-message');
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+    });
+
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+    });
+
+    loginButton.addEventListener('click', () => {
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        if (!email || !password) {
+            errorMessage.innerText = "Não pode deixar em branco";
+            errorMessage.style.display = "block";
+        } else {
+            errorMessage.style.display = "none";
+            // Lógica para login, caso ambos os campos estejam preenchidos
+        }
+    });
 });
